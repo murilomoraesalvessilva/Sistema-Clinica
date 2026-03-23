1efb5e387dc424af820a253d238cf46a8e8fb8f6
murilomoraesalvessilva <murilomoraesgithub@gmail.com>
2026-03-23 15:47:08 -0300

    Incremento de SQLiteMedicoRepository

---
+++ src/Infrastructure/Database/SQLiteMedicoRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Infrastructure\Database;
+
+use App\Domain\Medico\Medico;
+use App\Domain\Medico\MedicoRepositoryInterface;
+
+class SQLiteMedicoRepository implements MedicoRepositoryInterface {
+    public function __construct(private PDO $pdo) {
+        $this->pdo = $pdo
+    }
+
+    public function salvar(Medico $medico): void {
+        $stmt = $this->pdo->prepare("
+            INSERT INTO medicos (nome, crm, especialidade)
+            VALUES (:nome, :crm, :especialidade)
+        ");
+
+        $stmt->execute([
+            ':nome' => $medico->getNome(),
+            ':crm' => $medico->getCrm(),
+            ':especialidade' => $medico->getEspecialidade()
+        ]);
+    }
+    public function buscarPorID(int $id): ?Medico {
+        $stmt = $this->pdo->prepare("
+            SELECT * FROM medicos WHERE id = :id
+        ");
+
+        $stmt->execute([':id' => $id]);
+        $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$dados) {
+            return null;
+        }
+
+        return new Medico(
+            $dados['nome'], 
+            $dados['crm'], 
+            $dados['especialidade']
+            );
+    }
+
+    public function listarTodos(): array {
+        $stmt = $this->pdo->query("SELECT * FROM medicos");
+        $medicos = [];
+
+        foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $dados) {
+            $medicos[] = new Medico(
+                $dados['nome'],
+                $dados['crm'],
+                $dados['especialidade']
+            );
+        }
+
+        return $medicos;
+
+    }
+    public function deletar(int $id): void {
+        $stmt = $this->pdo->prepare("
+            DELETE FROM medicos WHERE id = :id
+        ");
+
+        $stmt->execute([':id' => $id]);
+    }
+}
